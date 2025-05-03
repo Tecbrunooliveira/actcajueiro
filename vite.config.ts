806d8277e748bdf0fa016e5c54d4d07f2bdde8ae
Tecbrunooliveira <tecoliveirabruno@gmail.com>
2025-05-03 11:45:58 -0300

    feat: transforma aplicação em PWA com vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +14,26 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ACT - CAJUEIRO',
+        short_name: 'CAJUEIRO',
+        description: 'Aplicação PWA do ACT - CAJUEIRO',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+          // Adicione outros ícones se desejar
+        ],
+      },
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
