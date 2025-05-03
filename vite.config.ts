949f5fb9c28d434ec6813853e61fb5ec7145c784
Tecbrunooliveira <tecoliveirabruno@gmail.com>
2025-05-03 14:58:11 -0300

    fix: remove imports indevidos do vite.config.ts e ajustes para build do react-pdf

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { VitePWA } from 'vite-plugin-pwa';
-import React from 'react';
-import { Document, Page } from 'react-pdf';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
